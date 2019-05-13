14fa7fa32c39a33e9289acd65216047ab8a89c5d
Alex Holanda <alex.holanda@outlook.com>
2019-05-13 13:27:43 -0300

    Update

--- src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -11,21 +11,16 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import com.algaworks.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	@Autowired
-	private AlgamoneyApiProperty algamoneyApiProperty;
-	
-	private String origemPermitida = algamoneyApiProperty.getOriginPermitida();
+	private String origemPermitida = ""; //TODO : configurar prod
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
--- src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/algaworks/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -4,7 +4,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -17,14 +16,9 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import com.algaworks.algamoney.api.config.property.AlgamoneyApiProperty;
-
 @ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
 
-	@Autowired
-	private AlgamoneyApiProperty algamoneyApiProperty;
-	
 	@Override
 	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
 
@@ -57,7 +51,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
 		
 		refreshTokenCookie.setHttpOnly(true);
-		refreshTokenCookie.setSecure(algamoneyApiProperty.getSeguranca().getEnableHttps());
+		refreshTokenCookie.setSecure(true); // TODO : mudar
 		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
 		refreshTokenCookie.setMaxAge(2592000);
 		
