4e156c340ed878dcffb5307c43dedb1689f4eca5
Alex Holanda <alex.holanda@outlook.com>
2019-07-08 08:56:27 -0300

    Correção de bugs

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -32,13 +32,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		clients.inMemory()
 					.withClient("angular").secret("@ngul@r0").scopes("read", "write")
 					.authorizedGrantTypes("password", "refresh_token")
-					.accessTokenValiditySeconds(1800)
-					.refreshTokenValiditySeconds(3600 * 24)
+					.accessTokenValiditySeconds(30)
+					.refreshTokenValiditySeconds(3600)
 				.and()
 					.withClient("mobile").secret("m0b1l30").scopes("read")
 					.authorizedGrantTypes("password", "refresh_token")
-					.accessTokenValiditySeconds(1800)
-					.refreshTokenValiditySeconds(3600 * 24);
+					.accessTokenValiditySeconds(30)
+					.refreshTokenValiditySeconds(3600);
 	}
 	
 	@Override
